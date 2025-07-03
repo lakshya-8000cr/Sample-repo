c433f1b9766e683e5cf29f7f0bffca562c534d70
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-07-03 12:32:41 +0530

    Added the comment lines

--- new.cpp
+++ new.cpp
@@ -1,80 +1,80 @@
+// // #include<iostream>
+// // using namespace std ;
+
+// // int main(){
+
+// //     int x ;
+// //     cin >> x;
+// //     int count = 0;
+
+// //     for(int i =2; i<=x; i++){
+// //         if(x%i==0){
+// //             count ++;
+// //         }
+// //     }
+
+// //     if(count==1){
+// //         cout << "Prime";
+// //     }else{
+// //         cout<<"Not Prime";
+// //     }
+
+// // }
+
+
+
+
+// //palendrom;
+// // #include<iostream>
+// // using namespace std ;
+
+// // int main(){
+// //     int reverse = 0;
+// //     int c;
+// //     cin>>c;
+// //     int original = c ;
+
+// //     while(c!=0){
+// //         int remainder = c % 10;
+// //         reverse = reverse*10 + remainder;
+// //         c = c / 10;
+// //     }
+
+// //     if(reverse==original){
+// //         cout<<"yes palendrom";
+// //     }else{
+// //         cout<<"Not palendrom";
+// //     }
+
+// //     return 0;
+
+// // }
+
+
+
+
+// //palendrom ';
 // #include<iostream>
+// #include<cstring>
 // using namespace std ;
 
 // int main(){
+//     string name ;
+//     getline(cin,name);
 
-//     int x ;
-//     cin >> x;
-//     int count = 0;
+//     int i = 0;
 
-//     for(int i =2; i<=x; i++){
-//         if(x%i==0){
-//             count ++;
+//     int len =  name.length();
+//     int j = len - 1;
+
+//     while(i<j){
+//         if(name[i++]!=name[j--]){
+//             cout<<"Not palendrom string";
+//             return 0;
+//         }else{
+//             cout<<"Palendrom string";
+//             return 0;
+            
 //         }
 //     }
-
-//     if(count==1){
-//         cout << "Prime";
-//     }else{
-//         cout<<"Not Prime";
-//     }
-
 // }
-
-
-
-
-//palendrom;
-// #include<iostream>
-// using namespace std ;
-
-// int main(){
-//     int reverse = 0;
-//     int c;
-//     cin>>c;
-//     int original = c ;
-
-//     while(c!=0){
-//         int remainder = c % 10;
-//         reverse = reverse*10 + remainder;
-//         c = c / 10;
-//     }
-
-//     if(reverse==original){
-//         cout<<"yes palendrom";
-//     }else{
-//         cout<<"Not palendrom";
-//     }
-
-//     return 0;
-
-// }
-
-
-
-
-//palendrom ';
-#include<iostream>
-#include<cstring>
-using namespace std ;
-
-int main(){
-    string name ;
-    getline(cin,name);
-
-    int i = 0;
-
-    int len =  name.length();
-    int j = len - 1;
-
-    while(i<j){
-        if(name[i++]!=name[j--]){
-            cout<<"Not palendrom string";
-            return 0;
-        }else{
-            cout<<"Palendrom string";
-            return 0;
-            
-        }
-    }
-}
