992a2885ba9ca15fb273044616d77f34633eff05
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-07-14 12:18:14 +0530

    added the question 3

--- OOPS/areaparamter.cpp
+++ OOPS/areaparamter.cpp
@@ -6,5 +6,48 @@
 using namespace std ;
 
 class Rectangle {
+
+    public :
+    int length ;
+    int width ;
     
+    Rectangle(int length,int width){
+        this->length = length;
+        this->width = width ;
+    }
+
+    int area;
+    int peri;
+
+    void Area(){
+         area = length * width ;
+    }
+
+    void Perimeter(){
+         peri = 2 * (length+width);
+    }
+
+    void detail(){
+        cout<<"Area: "<<area <<endl;
+        cout<<"Perimetter: "<<peri<<endl;
+    }
+
+    ~Rectangle(){};
+};
+
+
+int main(){
+    int area ;
+    cin>>area;
+    int width;
+    cin>>width;
+
+    Rectangle R(area,width);
+    R.Area();
+    R.Perimeter();
+
+    R.detail();
+
+    return 0;
+
 }
