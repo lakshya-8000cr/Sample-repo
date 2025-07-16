8843a1eec5193e7e2efc7e5ed2af6a94d26eb8e2
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-07-16 11:31:11 +0530

    added the questions

---
+++ .cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#define PI_MACRO 3.14159 // used for defined macros 
+
+const double PI_CONST = 3.14159; // used for PI constant 
+
+constexpr double PI_CONSTEXPR = 3.14159;
+
+int main() {
+    double radius = 5.0;
+
+    double area_macro = PI_MACRO * radius * radius;
+
+
+    double area_const = PI_CONST * radius * radius;
+
+
+    double area_constexpr = PI_CONSTEXPR * radius * radius;
+
+    cout << "Using #define macro     : Area = " << area_macro << endl;
+    cout << "Using const variable    : Area = " << area_const << endl;
+    cout << "Using constexpr keyword : Area = " << area_constexpr << endl;
+
+    return 0;
+}
